c5c656b508b83d022b35a1fe469293c8a98cb142
Wilker Lisboa <wilkerlisboa.ofc@gmail.com>
2021-11-22 11:27:58 -0300

    First commit

---
+++ script.js
@@ -0,0 +1,12 @@
+/*
+var menu = document.querySelector(".button_menu");
+
+menu.onclick = function efeito(){
+   var b = document.getElementById("lista");
+   b.style.animation = "none";
+   setTimeout(function(){
+      b.style.animation = "";
+   }, 100);
+}
+
+*/
